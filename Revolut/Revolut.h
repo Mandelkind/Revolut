3659a25c19e3d0789272b1cbb567f1bc241c2227
Marco Betschart <marco@mandelkind.swiss>
2018-03-02 13:43:58 +0100

    Initial Commit

---
+++ Revolut/Revolut.h
@@ -0,0 +1,19 @@
+//
+//  Revolut.h
+//  Revolut
+//
+//  Created by Marco Betschart on 02.03.18.
+//  Copyright © 2018 MANDELKIND. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Revolut.
+FOUNDATION_EXPORT double RevolutVersionNumber;
+
+//! Project version string for Revolut.
+FOUNDATION_EXPORT const unsigned char RevolutVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Revolut/PublicHeader.h>
+
+
